ae8f761e36fe7c5bf787206432037726ebfb4f0d
Ohiduzzaman Shuvo <oh599627@dal.ca>
2022-09-07 00:57:13 -0300

    Implemented setState Example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ClassBasedComponents from './components/ClassBasedComponent';
-import FunctionalBasedComponent from './components/FunctionalBasedComponent';
+//import FunctionalBasedComponent from './components/FunctionalBasedComponent';
 
 function App() {
   return (
     <div className="App">
      < ClassBasedComponents/>
-      <FunctionalBasedComponent/>
+      {/*<FunctionalBasedComponent/>*/}
     </div>
   );
 }
--- src/components/ClassBasedComponent.jsx
+++ src/components/ClassBasedComponent.jsx
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 
 
 class ClassBasedComponent extends Component {
+  state={
+    //write all state properties inside of this
+    count:0 //initial value of count
+  }
+  handleClick=()=>{
+    const {count}=this.state;
+    this.setState({
+      count: count+1
+    },()=>{
+      console.log(this.state)
+    })
+  }
   render() {
-    return (<div>Class based component example</div>);
+    console.log(this.state)
+    return (<div>
+      <button onClick={this.handleClick}>Click</button>
+      {
+        this.state.count==5 && "count is 5"
+      }
+    </div>);
   }
 };
 export default ClassBasedComponent;
