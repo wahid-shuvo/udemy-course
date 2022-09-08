f5a231fa8ff9f4a7babf3ff822256e5ef2204c14
Ohiduzzaman Shuvo <oh599627@dal.ca>
2022-09-07 23:13:06 -0300

    Hooks Usestate and useEffect example

--- src/components/FunctionalBasedComponent.jsx
+++ src/components/FunctionalBasedComponent.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
-import PropExample from '../PropExample';
+import React, { useEffect, useState } from 'react';
+//usestate
+//useffect
+//usecontext
+//usereducer
+//useref
 
-
-const Child=()=>{
-  return(
-    <p>Child component</p>
-  )
-}
 
 function FunctionalBasedComponent() {
 
-  const parentComponentHandler=()=>{
-    console.log('Hello from parent component')
-  }
-  const getValueFromChildComponent=(value)=>{
-    console.log(value)
-  }
+  const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(false);
 
-  return (<div className="App">
-    <Child/>
-    Functional based component
-    <PropExample getValueFromChildComponent={getValueFromChildComponent}/>
-  </div>);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    console.log('Effect is called once');
+  }, []); //Work Like componentDidMount in the class based component.That is this function is called on page load
+
+  useEffect(() => {
+    if (count === 5) {
+      setFlag(true);
+    }
+  }, [count]); // componentDidUpdate
+
+  useEffect(()=>{
+    return ()=>{
+      console.log('unmounted')
+    }
+  }) // Component Will unmount
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click</button>
+      Counter Value is {count}
+      {
+        flag && <p>Hello</p>
+      }
+    </div>);
 
 }
-export default FunctionalBasedComponent
+
+export default FunctionalBasedComponent;
 
