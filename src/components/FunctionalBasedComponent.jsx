50a3ffed901dcc15250527039dfab66473f920d4
Ohiduzzaman Shuvo <oh599627@dal.ca>
2022-09-17 15:18:42 -0300

    Added Usereducer Example

--- src/components/FunctionalBasedComponent.jsx
+++ src/components/FunctionalBasedComponent.jsx
@@ -1,22 +1,39 @@
-import React, { useContext } from 'react';
-import { Context } from '../App';
 //usestate
 //useffect
 //usecontext
 //usereducer
 //useref
 
+import React,{ useReducer } from 'react';
+
+const intialState = {
+  flag: false,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_BUTTON':
+      return (
+        {
+          ...state,
+          flag:!state.flag
+        }
+
+      )
+    default:
+      return state;
+
+  }
+};
 
 function FunctionalBasedComponent() {
 
-  const getValueFromContext=useContext(Context)
-  console.log(getValueFromContext);
+  const [state, dispatch] = useReducer(reducer, intialState);
+  console.log(state)
   return (
     <div>
-      <button style={{ backgroundColor: getValueFromContext }}>Click</button>
+      <button onClick={() => dispatch({ type: 'TOGGLE_BUTTON' })}>Toggle</button>
     </div>
   );
-
 
 }
 
